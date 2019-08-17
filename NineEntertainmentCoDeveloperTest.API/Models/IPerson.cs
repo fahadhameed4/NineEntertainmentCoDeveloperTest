ef9c944fca13f51415f2cf02d015236ca7c06f85
Fahad Hameed <fahadhameed160@hotmail.com>
2019-08-18 01:28:55 +1000

    API project has been added

---
+++ NineEntertainmentCoDeveloperTest.API/Models/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineEntertainmentCoDeveloperTest.API.Models
+{
+    /// <summary>
+    /// This is person interface 
+    /// </summary>
+   public interface IPerson
+    {
+         string Name { get; set; }
+         int Age { get; set; }
+         string ToString();
+    }
+}
