ec5d5540c1ad19af70b3d7d16c182458fa3ecd5d
Fahad Hameed <fahadhameed160@hotmail.com>
2019-08-18 22:28:24 +1000

    Repositories have been added

---
+++ NineEntertainmentCoDeveloperTest.API/Repositories/IPersonRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineEntertainmentCoDeveloperTest.API.Models
+{
+    /// <summary>
+    /// This is person interface Repository 
+    /// </summary>
+   public interface IPersonRepository
+    {
+        Task<IEnumerable<IPerson>> InitPeople();
+        bool AddPeople<T>();
+        int NumberofPeople<T>(List<T> lisitofpeople);
+        int AverageAge<T>(List<T> lisitofpeople);
+        int MaxAge<T>(List<T> lisitofpeople);
+        int MinAge<T>(List<T> lisitofpeople);
+        int Count<T>(List<T> lisitofraces);
+    }
+}
---
+++ NineEntertainmentCoDeveloperTest.API/Repositories/PersonRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NineEntertainmentCoDeveloperTest.API.Models
+{
+    /// <summary>
+    /// This is person repository 
+    /// </summary>
+    public class PersonRepository
+    {
+        private readonly IPerson _person;
+        
+        public PersonRepository(IPerson person)
+        {
+            this._person = person;
+        }
+
+        public Person  AddRace()
+        {
+                return this._person;
+        }
+
+        public static List<Person> InitPeople()
+        {
+            List<Person> people = new List<Person>();
+            Random rnd = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                people.Add(new Person()
+                {
+                    Name = "Person #" + i.ToString(),
+                    Age = rnd.Next(1, 99)
+                });
+            }
+            return people;
+        }
+
+
+    }
+       
+}
